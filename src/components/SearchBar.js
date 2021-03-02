918737f5f03271799920ffe38a1cb109223176f0
bryanjug <bryandjug@gmail.com>
2021-03-02 01:56:14 -0800

    added communication from child to parent

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,16 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = {term: ''};
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form" action="">
+                <form className="ui form" action="" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label htmlFor="">Image Search</label>
                         <input type="text" value={this.state.term} onChange={e => this.setState({term: e.target.value})} />
